1d6e331dd4c5641122cb2158a1ec967adaddfa52
Sebastian Werner <swerner@zynga.com>
2011-09-15 16:00:52 +0200

    Added Style and Engine API. Worked on migration.

---
+++ src/Engine.js
@@ -0,0 +1,41 @@
+/* 
+==================================================================================================
+  
+	Copyright Zynga Inc
+
+--------------------------------------------------------------------------------------------------
+
+	Based on the work of:
+	Jasy JavaScript Tooling Framework (http://github.com/wpbasti/jasy)
+	License: MIT + Apache (V2)
+
+==================================================================================================
+*/
+
+if (!window.zynga) {
+	zynga = {};
+}
+
+zynga.Engine = {
+	
+	NAME : (function(global) {
+
+		var engine;
+		var doc = global.document;
+		var docStyle = doc.documentElement.style;
+
+		if (global.opera && Object.prototype.toString.call(opera) === '[object Opera]') {
+			engine = 'presto';
+		} else if ('MozAppearance' in docStyle) {
+			engine = 'gecko';
+		} else if ('WebkitAppearance' in docStyle) {
+			engine = 'webkit';
+		} else if (typeof navigator.cpuClass === 'string') {
+			engine = 'trident';
+		}
+
+		return engine;
+
+	}(this))
+	
+};
