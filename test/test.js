73b88474ce7970df4838f55d3e988eee6a70c4ad
Sebastian Werner <swerner@zynga.com>
2011-10-11 17:08:58 +0200

    Added more tests

--- test/test.js
+++ test/test.js
@@ -146,3 +146,45 @@ test("Scrolling + Zooming + Scrolling via API", function() {
 	equal(values.zoom, 1.7);
 
 });
+
+test("Snapping", function() {
+	
+	var scroller = new Scroller(null, {
+		snapping: true
+	});
+	
+	scroller.setDimensions(1000, 600, 5000, 5000);
+	scroller.setSnapSize(50, 100);
+	
+	scroller.scrollTo(200, 400);
+	var values = scroller.getValues();
+	equal(values.left, 200);
+	equal(values.top, 400);
+
+	scroller.scrollTo(237, 124);
+	var values = scroller.getValues();
+	equal(values.left, 250);
+	equal(values.top, 100);
+	
+});
+
+
+test("Paging", function() {
+	
+	var scroller = new Scroller(null, {
+		paging: true
+	});
+	
+	scroller.setDimensions(1000, 600, 5000, 5000);
+	
+	scroller.scrollTo(1000, 600);
+	var values = scroller.getValues();
+	equal(values.left, 1000);
+	equal(values.top, 600);
+
+	scroller.scrollTo(1400, 1100);
+	var values = scroller.getValues();
+	equal(values.left, 1000);
+	equal(values.top, 1200);
+	
+});
